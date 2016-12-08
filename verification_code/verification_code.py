af312c430ccfc663caa4a9ceae65639fe35959d8
zhaoxin <zhaoxin_dlut@163.com>
2016-12-08 19:53:58 +0800

    生成****-****-****-****格式的验证码

--- verification_code/verification_code.py
+++ verification_code/verification_code.py
@@ -4,20 +4,35 @@
 #脚本涉及到两个标准库random，简单教程：http://fulerbakesi.iteye.com/blog/1589097
 #string，简单教程：http://www.cnblogs.com/iwalkfreely/p/4714866.html,   http://biancheng.dnbcw.info/python/455071.html
 
+#生成****-****-****-****格式的激活码
+
 import random, string
-import cPickle as p
+import os
+
+#存储器，实现数据的存取
+#p.dump(保存内容, 文件对象)
+#p.load(文件对象)
+#import cPickle as p  
 
 result = 'Verification_codes.txt'
-def random_str(num, length = 7):
-    f = open(result, 'wb')
-    for i in range(num):
-        chars = string.letters + string.digits
-        s = [random.choice(chars) for join in range(length)]
-        #s.insert(0, str(i+1))
-        #print ''.join(s)
-        #p.dump(''.join(s), f)  #保存结果在格式上存在问题
-        f.write(''.join(s) + '\n')
-    f.close()
+#基础字母和数字
+chars = string.letters + string.digits
+
+#获得四个字母和数字的随机组合
+def getRandom():
+    return ''.join(random.sample(chars, 4))
+
+#每个验证码由四段四元组组成
+def construct(group=4):
+    return '-'.join([getRandom() for i in range(group)])
+
+def generate(num):
+    return [construct() for i in range(num)]
 
 if __name__ == '__main__':
-    random_str(200)
+    f = open(result, 'w')
+    codes = generate(200)
+    code = os.sep.join(codes)
+    print code
+    f.write(os.sep.join(codes))
+    f.close()
